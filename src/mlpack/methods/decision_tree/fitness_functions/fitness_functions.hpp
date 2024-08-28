355762f136ddd8a6abf41fbe7c06f45b49cc78a1
RE-NY <rksamadkhani@gmail.com>
2024-08-29 02:32:08 +0530

    Added STB library and updated CMake configuration

--- src/mlpack/methods/decision_tree/fitness_functions/fitness_functions.hpp
+++
@@ -1,4 +0,0 @@
-#include "gini_gain.hpp"
-#include "information_gain.hpp"
-#include "mad_gain.hpp"
-#include "mse_gain.hpp"
--- src/mlpack/methods/decision_tree/select_functions/select_functions.hpp
+++
@@ -1,3 +0,0 @@
-#include "all_dimension_select.hpp"
-#include "multiple_random_dimension_select.hpp"
-#include "random_dimension_select.hpp"
